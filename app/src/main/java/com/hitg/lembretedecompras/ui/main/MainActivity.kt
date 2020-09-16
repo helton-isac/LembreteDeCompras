e03573c7b7c051f4c0ae53eba7fc01ceaf942072
Helton Isac <helton.isac@gmail.com>
2020-09-15 22:28:48 -0300

    Creating the Adapter

--- app/src/main/java/com/hitg/lembretedecompras/ui/main/MainActivity.kt
+++ app/src/main/java/com/hitg/lembretedecompras/ui/main/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.hitg.lembretedecompras.ui.main
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.hitg.lembretedecompras.R
 
 class MainActivity : AppCompatActivity() {
@@ -9,5 +11,10 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val recyclerView = findViewById<RecyclerView>(R.id.rvProdutos)
+        val adapter = ProdutoListaAdapter(this)
+        recyclerView.adapter = adapter
+        recyclerView.layoutManager = LinearLayoutManager(this)
     }
 }
